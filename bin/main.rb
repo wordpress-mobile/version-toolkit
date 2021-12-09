28bce380ea020c6ace9e2caea2ef2ec695715130
Oguz Kocer <oguz.kocer@automattic.com>
2021-12-08 20:37:16 -0500

    Implements adding a submodule update commit to a branch

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'octokit'
-require_relative '../lib/Helper.rb'
+require_relative '../lib/Helper'
 
 test_repository = 'oguzkocer/version-test-bin'
 test_label = 'bug'
@@ -11,8 +11,7 @@ client.auto_paginate = true
 
 test_repo_helper = Helper.new(client, test_repository)
 
-#puts test_repo_helper.get_branch_names()
-#puts test_repo_helper.pull_requests_with_label(test_label)
-
-#sha = test_repo_helper.default_branch_sha()
-#test_repo_helper.create_branch(sha)
+test_repo_helper.create_submodule_hash_update_commit('heads/created_from_helper',
+                                                     'gutenberg',
+                                                     'fc8a7891f900aa67a859b28f01aeb4e24fbf9011',
+                                                     'Update gutenberg submodule hash')
--- lib/helper.rb
+++ lib/helper.rb
@@ -2,6 +2,9 @@
 
 require 'octokit'
 
+CREATE_TREE_MODE_SUBMODULE_COMMIT = '160000'
+CREATE_TREE_TYPE_COMMIT = 'commit'
+
 class Helper
   def initialize(client, repo)
     @client = client
@@ -23,11 +26,39 @@ class Helper
     return branches.map { |branch| branch.name }
   end
 
-  def create_branch(sha)
-    @client.create_ref(@repo, "heads/created_from_helper", sha)
+  def create_branch(branch_name, sha)
+    @client.create_ref(@repo, branch_name, sha)
   end
 
   def default_branch_sha()
-    return @client.ref(@repo, "heads/trunk").object.sha
+    return branch_sha("heads/trunk")
+  end
+
+  def branch_sha(branch)
+    return @client.ref(@repo, branch).object.sha
+  end
+
+  def create_submodule_hash_update_commit(branch, submodule_path, new_submodule_hash, commit_message)
+    parent_sha = branch_sha(branch)
+    tree_sha = create_submodule_hash_update_tree(parent_sha, submodule_path, new_submodule_hash)
+    new_commit_sha = create_standalone_commit(commit_message, tree_sha, parent_sha)
+    return @client.update_ref(@repo, branch, new_commit_sha, false).object.sha
+  end
+
+  def create_standalone_commit(commit_message, tree, parent_sha)
+    commit = @client.create_commit(@repo, commit_message, tree, parent_sha)
+    return commit.sha
+  end
+
+  def create_submodule_hash_update_tree(base_tree, submodule_path, new_submodule_hash)
+    tree = @client.create_tree(@repo, [{
+      :path => submodule_path,
+      :mode => CREATE_TREE_MODE_SUBMODULE_COMMIT,
+      :type => CREATE_TREE_TYPE_COMMIT,
+      :sha => new_submodule_hash
+    }],
+      :base_tree => base_tree,
+    )
+    return tree.sha
   end
 end
